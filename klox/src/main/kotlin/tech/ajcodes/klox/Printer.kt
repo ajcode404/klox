bc9feba010d336e29eb63fe59bebeadcf6388a10
AJ <138494702+ajcode404@users.noreply.github.com>
2024-03-05 21:06:13 +0530

    Parsing tool (#3)
    * parse tool
    
    * added visitor
    
    * printer visitor

---
+++ klox/src/main/kotlin/tech/ajcodes/klox/Printer.kt
@@ -0,0 +1,48 @@
+package tech.ajcodes.klox
+
+import kotlin.math.exp
+
+class AstPrinter : Visitor<String> {
+
+    fun print(expr: Expr) = expr.accept(this)
+
+    override fun visitBinaryExpr(expr: Binary): String {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right)
+    }
+
+   override fun visitGroupingExpr(expr: Grouping): String {
+      return parenthesize("group", expr.expression)
+   }
+
+    override fun visitLiteralExpr(expr: Literal): String {
+        return expr.value?.toString() ?: "nil"
+    }
+
+    override fun visitUnaryExpr(expr: Unary): String {
+        return parenthesize(expr.operator.lexeme, expr.right)
+    }
+
+    private fun parenthesize(name: String, vararg exprs: Expr): String {
+        return buildString {
+            append("(").append(name)
+            exprs.forEach {
+                append(" ")
+                append(it.accept(this@AstPrinter))
+            }
+            append(")")
+
+        }
+    }
+}
+
+fun main() {
+    val expression = Binary(
+        Unary(
+            Token(TokenType.MINUS, "-", null, 1),
+            Literal(123)
+        ),
+        Token(TokenType.STAR, "*", null, 1),
+        Grouping(Literal(45.67))
+    )
+    println(AstPrinter().print(expression))
+}
---
+++ klox/src/main/kotlin/tech/ajcodes/tools/GenerateAst.kt
@@ -0,0 +1,67 @@
+package tech.ajcodes.tools
+
+import java.io.PrintWriter
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    if (args.size != 1) {
+        System.err.println("Usage: generate_ast <output_directory>")
+        exitProcess(64)
+    }
+    val outputDir = args[0]
+    defineAst(outputDir, "Expr", arrayOf(
+        "Binary   - val left: Expr,val operator: Token ,val right: Expr",
+        "Grouping - val expression: Expr",
+        "Literal  - val value: Any?",
+        "Unary    - val operator: Token ,val right: Expr"
+    ))
+}
+
+fun defineAst(outputDir: String, baseName: String, types: Array<String>) {
+    val path = "$outputDir/$baseName.kt"
+    val writer = PrintWriter(path, "UTF-8")
+
+    writer.println("package tech.ajcodes.klox")
+    writer.println()
+    writer.println("sealed class $baseName {")
+    // the base accept() method.
+    writer.println()
+    writer.println("\tabstract fun <R> accept(visitor: Visitor<R>): R")
+
+    writer.println("}")
+
+    writer.println()
+    // define visitor
+    defineVisitor(writer, baseName, types)
+
+    // define type
+    types.forEach {
+        val className = it.split("-")[0].trim()
+        val fields = it.split("-")[1].trim()
+        """
+            
+            data class $className($fields): $baseName() {
+                override fun <R> accept(visitor: Visitor<R>): R {
+                    return visitor.visit$className$baseName(this)
+                }
+            }
+        """.trimIndent().let {
+            writer.println(it)
+        }
+    }
+    writer.println()
+    writer.close()
+}
+
+
+
+fun defineVisitor(writer: PrintWriter, baseName: String, types: Array<String>) {
+    writer.println("interface Visitor<R> {")
+    types.forEach {
+        val typeName = it.split("-")[0].trim()
+        writer.println("\tfun visit$typeName$baseName(${baseName.lowercase()}: $typeName): R")
+    }
+    writer.println("}")
+}
+
+
